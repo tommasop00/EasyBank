48b142933b64f1099299b33b296965ecb32fa32b
Tommaso <parrinit@ergmeeting.it>
2020-11-24 23:49:50 +0100

    Eliminati Errori di Clion

--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -38,8 +38,9 @@ if(NOT DEFINED CMAKE_CROSSCOMPILING)
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  # Include the install script for the subdirectory.
+  # Include the install script for each subdirectory.
   include("/home/tommaso/Scrivania/CLionProject/EasyBank/cmake-build-debug/test/cmake_install.cmake")
+
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
